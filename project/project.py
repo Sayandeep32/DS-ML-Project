af7e1fb863c8f1254445f0740759a69d1e408a9d
Sayandeep32 <sayandeepkundu17@gmail.com>
2025-06-24 20:38:19 +0530

    Update project.py

--- project/project.py
+++ project/project.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
